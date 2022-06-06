753ba81bccd0b31a9dab735c6bc77f36185b3936
Camilli Joannes <90011816+CamilliJ@users.noreply.github.com>
2022-06-06 12:17:16 -0300

    Add files via upload

---
+++ ListView/ViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ViewCell.swift
+//  ListView
+//
+//  Created by COTEMIG on 06/06/22.
+//
+
+import UIKit
+
+class ViewCell: UITableViewCell {
+
+    @IBOutlet weak var nomeLabel: UILabel!
+    @IBOutlet weak var imagemView: UIImageView!
+    @IBOutlet weak var descricaoLabel: UILabel!
+    
+}
--- ListView/ViewController.swift
+++ ListView/ViewController.swift
@@ -12,24 +12,26 @@ class ViewController: UIViewController , UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
+    let ArrayNome: [String] = ["Gastly", "Ditto", "Psyduck", "Charmander", "Jigglypuff", "Pikachu", "Wingull"]
+    let ArrayImagens = ["bolaroxa", "ditto", "duck", "fogo", "giglipuff", "pika", "pombo"]
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return ArrayNome.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Título Label"
-        
-        if indexPath.row == 4{
-            cell.textLabel?.text = "Texto diferente"
-            cell.textLabel?.textColor = UIColor.purple
-        }
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ViewCell
+        cell.nomeLabel?.text = ArrayNome[indexPath.row]
+        cell.descricaoLabel?.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+        let nomeimagem = ArrayImagens[indexPath.row]
+        cell.imagemView?.image = UIImage(named: nomeimagem)
         return cell
     }
 
